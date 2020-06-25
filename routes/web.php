5206cced5a802f3ae9b20234c86f0a920375f45a
Carlos Eduardo Leal Thomazella <caduthomazella@hotmail.com>
2020-06-25 12:15:26 -0300

    rotas e testes com controller

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::middleware([])->group(function() {
+
+    Route::prefix('admin')->group(function() {
+
+        Route::namespace('Admin')->group(function() {
+            
+            Route::get('/dashboard', 'TesteController@teste')->name('admin.dashboard');
+        
+            Route::get('/financeiro', 'TesteController@teste')->name('admin.financeiro');
+            
+            Route::get('/produtos', 'TesteController@teste')->name('admin.products');
+
+            Route::get('/', 'TesteController@teste')->name('admin.home');
+
+        });        
+    });
+});
+
+Route::get('/produtos/{idProduct}', function ($idProduct = '') {
+    return "Produtos(s) {$idProduct}";
+});
+
+Route::get('/categorias/{flag}', function ($flag) {
+    return "Produtos de categoria: {$flag}";
+});
+
+Route::match(['post', 'get'], '/match', function () {
+    return 'match';
+});
+
+Route::get('/contato', function () {
+    return view('contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
